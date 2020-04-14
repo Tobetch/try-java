198eeef0ea34e7948d62b59c6210c4d6c1fa93bc
Tobetch <takutobe@gmail.com>
2020-04-14 13:24:42 +0900

    remove unnecessary import sentence

--- spring-boot-crud/baseball/src/main/java/com/example/baseball/controller/PlayerController.java
+++ spring-boot-crud/baseball/src/main/java/com/example/baseball/controller/PlayerController.java
@@ -3,12 +3,10 @@ package com.example.baseball.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.baseball.domain.Player;
