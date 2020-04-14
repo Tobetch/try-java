9a40112daae2fa66bf7a31f0fff005a3ee06c1bd
Tobetch <takutobe@gmail.com>
2020-04-14 12:49:15 +0900

    add baseball

---
+++ spring-boot-crud/baseball/src/main/java/com/example/baseball/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.baseball.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/players";
+    }
+}
