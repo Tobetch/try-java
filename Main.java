77a00caa6ec19acbf7722e02088016e267ff4347
Tobetch <takutobe@gmail.com>
2020-08-02 15:37:31 +0900

    fix Main

--- Main.java
+++ Main.java
@@ -3,7 +3,9 @@ public class Main{
     public static void main(String[] args) {
  
         int i=0, j=0;
+	String name = "WASSHOI";
         System.out.println(i);
         System.out.println(j);
+	System.out.println(name);
     }
 }
