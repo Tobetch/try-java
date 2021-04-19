0a38c8e24e7b86e6553cfd3db8b5d2270274b753
Tobetch <takutobe@gmail.com>
2021-04-19 23:39:57 +0900

    delete mybatis-generator

--- try-mybatis-generator/README.md
+++
@@ -1,5 +0,0 @@
-- 参考
-https://qiita.com/ketman55/items/abcc1e23dcac0cc268b5
-
-- 公式サイト
-https://mybatis.org/generator/
--- try-mybatis-generator/src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS try_mybatis_generator (
-  id bigint(20) NOT NULL AUTO_INCREMENT,
-  column1 varchar(255) DEFAULT NULL,
-  column2 varchar(255) DEFAULT NULL,
-  column3 varchar(255) DEFAULT NULL,
-  PRIMARY KEY (id)
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
